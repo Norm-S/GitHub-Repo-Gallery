297c470f83c7069e649fb3bbf75ed47a020b6b7c
shum_n <shumnorman@gmail.com>
2024-02-21 15:59:43 -0800

    Add API fetch function

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 const overview = document.querySelector(".overview");
 
 const username = "Norm-S";
+
+const fetchUserProfile = async function () {
+  const res = await fetch(`https://api.github.com/users/${username}`);
+  const data = await res.json();
+
+  console.log(data);
+};
+
+fetchUserProfile();
